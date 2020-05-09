3a4c255dbc02cddf5d1530a88da18e89f63f6c9e
Daniel <kolodani95@gmail.com>
2020-05-09 23:18:33 +0200

    Oauth2 working

--- src/main/kotlin/com/kotlin_portfolio/KotlinPortfolioApplication.kt
+++ src/main/kotlin/com/kotlin_portfolio/KotlinPortfolioApplication.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.stereotype.Repository
+import org.springframework.boot.autoconfigure.security.oauth2.*;
 
 @SpringBootApplication
 open class KotlinPortfolioApplication
