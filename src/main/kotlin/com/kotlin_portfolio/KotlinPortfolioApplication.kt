4ecf03cbfdedc4d9c11c986c0c2c341e184bbe7a
Daniel <kolodani95@gmail.com>
2020-05-23 22:51:51 +0200

    IEX integration

--- src/main/kotlin/com/kotlin_portfolio/KotlinPortfolioApplication.kt
+++ src/main/kotlin/com/kotlin_portfolio/KotlinPortfolioApplication.kt
@@ -2,11 +2,6 @@ package com.kotlin_portfolio
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.oauth2.core.user.OAuth2User
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
-import java.util.*
 
 
 @SpringBootApplication
@@ -14,5 +9,4 @@ open class KotlinPortfolioApplication
 
 fun main(args: Array<String>) {
     runApplication<KotlinPortfolioApplication>(*args)
-
 }
